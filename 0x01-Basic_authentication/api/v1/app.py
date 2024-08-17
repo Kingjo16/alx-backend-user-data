9d215e05b7fab20560b503b03c4e4c44d733966f
kingjo <kidusyohannesred@gmail.com>
2024-08-17 01:05:20 -0700

    new sol

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,63 +1,21 @@
 #!/usr/bin/env python3
-"""Module Api Authorizer and unauthorizer."""
-import os
-from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+"""Flask app module."""
 
-from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-
+from flask import Flask, jsonify
+from api.v1.views import index
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
-    auth = Auth()
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """Handel whihch are not found."""
-    return jsonify({"error": "Not found"}), 404
-
+app.register_blueprint(index.bp)
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Handle 401 unauthorized errors."""
+def unauthorized_error(error):
+    """Handle 401 Unauthorized errors."""
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """Handel Forbidden errors."""
+def forbidden_error(error):
+    """Handle 403 Forbidden errors."""
     return jsonify({"error": "Forbidden"}), 403
 
-
-@app.before_request
-def authenticate_user():
-    """Request a user authenticator."""
-    if auth:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-        ]
-        if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
-                abort(401)
-            if user is None:
-                abort(403)
-
-
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
